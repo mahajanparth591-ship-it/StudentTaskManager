b7069f67cd82f5e5152bcc4ba658e242c92e2ee0
mahajanparth591-ship-it <mahajanparth591@gmail.com>
2026-08-20 23:22:12 +0530

    initiate project

---
+++ script.js
@@ -0,0 +1,33 @@
+let tasks = [];
+
+function addTask()
+{
+    let input = document.getElementById("taskInput");
+
+    if(input.value == "")
+    {
+        return;
+    }
+
+    tasks.push(input.value);
+
+    input.value = "";
+
+    displayTasks();
+}
+
+function displayTasks()
+{
+    let list = document.getElementById("taskList");
+
+    list.innerHTML = "";
+
+    for(let i = 0; i < tasks.length; i++)
+    {
+        let li = document.createElement("li");
+
+        li.innerHTML = tasks[i];
+
+        list.appendChild(li);
+    }
+}
